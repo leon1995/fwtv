ccb3475025e5c74b2e7e9462a5054fcae2dcbdd3
Leon <y6q6ea9w@mail-proxy.org>
2023-03-17 13:30:17 +0100

    fix linters

--- fwtv/factorial.py
+++ fwtv/factorial.py
@@ -61,7 +61,7 @@ def convert_timestamp(timestamp: str) -> datetime.datetime:
 
 
 def get_errors(
-        start: datetime.datetime, end: datetime.datetime, attendances: LIST_JSON_RESPONSE, employees: LIST_JSON_RESPONSE
+    start: datetime.datetime, end: datetime.datetime, attendances: LIST_JSON_RESPONSE, employees: LIST_JSON_RESPONSE
 ) -> GET_ERRORS:
     preconditions: typing.Dict[str, typing.List[str]] = collections.defaultdict(list)
     errors: typing.Dict[str, typing.List[verifier.Error]] = collections.defaultdict(list)
--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -7,13 +7,13 @@ import pytest
 
 from fwtv import factorial
 
-DATA_DIR = pathlib.Path(__file__).parent.joinpath('data')
-EMPLOYEES = json.loads(DATA_DIR.joinpath('employees.json').read_text(encoding='utf-8'))
-all_correct_attendances = json.loads(DATA_DIR.joinpath('all_correct.json').read_text(encoding='utf-8'))
-hours_6 = json.loads(DATA_DIR.joinpath('6_hours_error.json').read_text(encoding='utf-8')) + all_correct_attendances
-hours_9 = json.loads(DATA_DIR.joinpath('9_hours_error.json').read_text(encoding='utf-8')) + all_correct_attendances
-hours_10 = json.loads(DATA_DIR.joinpath('10_hours_error.json').read_text(encoding='utf-8')) + all_correct_attendances
-precondition_ = json.loads(DATA_DIR.joinpath('precondition.json').read_text(encoding='utf-8')) + all_correct_attendances
+DATA_DIR = pathlib.Path(__file__).parent.joinpath("data")
+EMPLOYEES = json.loads(DATA_DIR.joinpath("employees.json").read_text(encoding="utf-8"))
+all_correct_attendances = json.loads(DATA_DIR.joinpath("all_correct.json").read_text(encoding="utf-8"))
+hours_6 = json.loads(DATA_DIR.joinpath("6_hours_error.json").read_text(encoding="utf-8")) + all_correct_attendances
+hours_9 = json.loads(DATA_DIR.joinpath("9_hours_error.json").read_text(encoding="utf-8")) + all_correct_attendances
+hours_10 = json.loads(DATA_DIR.joinpath("10_hours_error.json").read_text(encoding="utf-8")) + all_correct_attendances
+precondition_ = json.loads(DATA_DIR.joinpath("precondition.json").read_text(encoding="utf-8")) + all_correct_attendances
 
 
 @pytest.mark.asyncio
@@ -29,8 +29,8 @@ async def test_api_error(monkeypatch, code: int, error):
         async def __aexit__(self, *_, **kwargs):
             pass
 
-    async with factorial.FactorialApi(api_key='') as api:
-        monkeypatch.setattr(api.session, 'get', Response)
+    async with factorial.FactorialApi(api_key="") as api:
+        monkeypatch.setattr(api.session, "get", Response)
         with pytest.raises(error):
             await api.get_employees()
         with pytest.raises(error):
@@ -54,27 +54,30 @@ async def test_response_json(monkeypatch):
             return EMPLOYEES
 
     class EmployeeResponse(Response):
-
         @staticmethod
         async def json():
             return EMPLOYEES
 
     class AttendancesResponse(Response):
-
         @staticmethod
         async def json():
             return all_correct_attendances
 
-    async with factorial.FactorialApi(api_key='') as api:
-        monkeypatch.setattr(api.session, 'get', EmployeeResponse)
+    async with factorial.FactorialApi(api_key="") as api:
+        monkeypatch.setattr(api.session, "get", EmployeeResponse)
         assert await api.get_employees() == EMPLOYEES
-        monkeypatch.setattr(api.session, 'get', AttendancesResponse)
+        monkeypatch.setattr(api.session, "get", AttendancesResponse)
         assert await api.get_attendances() == all_correct_attendances
 
 
-@pytest.mark.parametrize("timestamp, expected", [("2023-01-01Z", datetime.datetime(2023, 1, 1)),
-                                                 ("2023-02-02T11ZZ", datetime.datetime(2023, 2, 2, 11)),
-                                                 ("2023-03-05T12:15ZZZ", datetime.datetime(2023, 3, 5, 12, 15))])
+@pytest.mark.parametrize(
+    "timestamp, expected",
+    [
+        ("2023-01-01Z", datetime.datetime(2023, 1, 1)),
+        ("2023-02-02T11ZZ", datetime.datetime(2023, 2, 2, 11)),
+        ("2023-03-05T12:15ZZZ", datetime.datetime(2023, 3, 5, 12, 15)),
+    ],
+)
 def test_convert_timestamp(timestamp: str, expected: datetime.datetime):
     actual = factorial.convert_timestamp(timestamp)
     assert actual == expected
@@ -84,44 +87,55 @@ scenario1 = (datetime.datetime(2023, 1, 5), datetime.datetime(2023, 3, 5), all_c
 
 
 @pytest.mark.parametrize("start, end, attendances, employees, error_count", [scenario1])
-def test_error_detection_all_ok(start: datetime.datetime,
-                                end: datetime.datetime,
-                                attendances: factorial.LIST_JSON_RESPONSE,
-                                employees: factorial.LIST_JSON_RESPONSE,
-                                error_count: typing.Tuple[int, int]):
+def test_error_detection_all_ok(
+    start: datetime.datetime,
+    end: datetime.datetime,
+    attendances: factorial.LIST_JSON_RESPONSE,
+    employees: factorial.LIST_JSON_RESPONSE,
+    error_count: typing.Tuple[int, int],
+):
     preconditions, errors = factorial.get_errors(start, end, attendances, employees)
     assert len(preconditions) == error_count[0]
     assert len(errors) == error_count[1]
 
 
-HOURS_6_SCENARIO = (datetime.datetime(2023, 1, 1),
-                    datetime.datetime(2023, 3, 13),
-                    hours_6,
-                    EMPLOYEES,
-                    1,
-                    'Attended more than 6 hours without a cumulated break of 30 min')
-HOURS_9_SCENARIO = (datetime.datetime(2023, 1, 1),
-                    datetime.datetime(2023, 3, 13),
-                    hours_9,
-                    EMPLOYEES,
-                    2,
-                    'Attended more than 9 hours without a cumulated break of 45 min')
-HOURS_10_SCENARIO = (datetime.datetime(2023, 1, 1),
-                     datetime.datetime(2023, 3, 15),
-                     hours_10,
-                     EMPLOYEES,
-                     2,
-                     'Attended more than 10 hours without a single break of 11 hours')
+HOURS_6_SCENARIO = (
+    datetime.datetime(2023, 1, 1),
+    datetime.datetime(2023, 3, 13),
+    hours_6,
+    EMPLOYEES,
+    1,
+    "Attended more than 6 hours without a cumulated break of 30 min",
+)
+HOURS_9_SCENARIO = (
+    datetime.datetime(2023, 1, 1),
+    datetime.datetime(2023, 3, 13),
+    hours_9,
+    EMPLOYEES,
+    2,
+    "Attended more than 9 hours without a cumulated break of 45 min",
+)
+HOURS_10_SCENARIO = (
+    datetime.datetime(2023, 1, 1),
+    datetime.datetime(2023, 3, 15),
+    hours_10,
+    EMPLOYEES,
+    2,
+    "Attended more than 10 hours without a single break of 11 hours",
+)
 
 
-@pytest.mark.parametrize("start, end, attendances, employees, error_count, reason",
-                         [HOURS_6_SCENARIO, HOURS_9_SCENARIO, HOURS_10_SCENARIO])
-def test_error_detection(start: datetime.datetime,
-                         end: datetime.datetime,
-                         attendances: factorial.LIST_JSON_RESPONSE,
-                         employees: factorial.LIST_JSON_RESPONSE,
-                         error_count: int,
-                         reason: str):
+@pytest.mark.parametrize(
+    "start, end, attendances, employees, error_count, reason", [HOURS_6_SCENARIO, HOURS_9_SCENARIO, HOURS_10_SCENARIO]
+)
+def test_error_detection(
+    start: datetime.datetime,
+    end: datetime.datetime,
+    attendances: factorial.LIST_JSON_RESPONSE,
+    employees: factorial.LIST_JSON_RESPONSE,
+    error_count: int,
+    reason: str,
+):
     preconditions, errors = factorial.get_errors(start, end, attendances, employees)
     assert len(preconditions) == 0
     assert len(errors) == len(employees)
@@ -131,19 +145,17 @@ def test_error_detection(start: datetime.datetime,
             assert err.reason == reason
 
 
-PRECONDITION_SCENARIO = (datetime.datetime(2023, 1, 1),
-                         datetime.datetime(2023, 3, 15),
-                         precondition_,
-                         EMPLOYEES,
-                         1)
+PRECONDITION_SCENARIO = (datetime.datetime(2023, 1, 1), datetime.datetime(2023, 3, 15), precondition_, EMPLOYEES, 1)
 
 
 @pytest.mark.parametrize("start, end, attendances, employees, error_count", [PRECONDITION_SCENARIO])
-def test_precondition_detection(start: datetime.datetime,
-                                end: datetime.datetime,
-                                attendances: factorial.LIST_JSON_RESPONSE,
-                                employees: factorial.LIST_JSON_RESPONSE,
-                                error_count: int):
+def test_precondition_detection(
+    start: datetime.datetime,
+    end: datetime.datetime,
+    attendances: factorial.LIST_JSON_RESPONSE,
+    employees: factorial.LIST_JSON_RESPONSE,
+    error_count: int,
+):
     preconditions, errors = factorial.get_errors(start, end, attendances, employees)
     assert len(preconditions) == len(employees)
     for precondition in preconditions.values():
